5349789d2013c70c9d1ea667b6f74866bcaa9c73
Pat Jacobs <musicbender@gmail.com>
2016-10-21 16:00:55 -0700

    attepting to integrate material ui

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+
+import FlatButton from 'material-ui/FlatButton';
 import '../scss/header.scss';
 
-const Header = () => {
+const Header = (props) => {
     return (
         <div className="header">
             <h1>YouTube Search</h1>
             <h2>A search app built with React, SCSS, and Webpack</h2>
+            <FlatButton label="Default" />
         </div> 
     )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -2,13 +2,24 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import $ from 'jquery';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import Header from './components/header.jsx';
 import SearchBar from './components/search-bar.jsx';
 import VideoList from './components/video-list.jsx';
 import VideoDetail from './components/video-detail.jsx';
+import {deepOrange500} from 'material-ui/styles/colors';
 
 const API_KEY = 'AIzaSyD0tW8Wi1eMDsk6WxqA-EO1_5MtbwyS0pc';
+injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+  palette: {
+    accent1Color: deepOrange500,
+  },
+});
 
 class App extends Component {
     constructor(props) {
@@ -30,22 +41,23 @@ class App extends Component {
             });
         });
     }
+
     
     render() {
         const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
         
         return (
-            <div>
+            <MuiThemeProvider muiTheme={muiTheme}>
                 <Header />
                 <SearchBar onSearchTermChange={videoSearch} />
-                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList 
                     onVideoSelect={selectedVideo => this.setState({selectedVideo})} 
                     videos={this.state.videos} 
                 />
-            </div>
-        );
+            </MuiThemeProvider>
+        )
     }
-}
+} 
 
 ReactDOM.render(<App />, document.querySelector('.container'));
