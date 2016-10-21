3b9d27d3f474e24a8470ee6badd3a43c4466da45
Pat Jacobs <musicbender@gmail.com>
2016-10-21 08:27:14 -0700

    added custom styling with scss

--- src/components/search-bar.jsx
+++ src/components/search-bar.jsx
@@ -4,24 +4,29 @@ import '../scss/search-bar.scss';
 class SearchBar extends Component {
     constructor(props) {
         super(props);
-        
+
         this.state = { term: '' };
     }
-    
+
     onInputChange(term) {
         this.setState({term});
         this.props.onSearchTermChange(term);
     }
-    
+
     render() {
         return (
-            <div className="search-bar">
+            <div className="input-group search-bar">
+                <span className="input-group-addon search-icon-container" id="basic-addon1"><i className="fa fa-search" aria-hidden="true"></i></span>
                 <input 
-                value={this.state.term}
-                onChange={e => this.onInputChange(e.target.value)} />
-            </div> 
+                    type="text" 
+                    className="form-control" 
+                    aria-describedby="basic-addon1"
+                    value={this.state.term}
+                    onChange={e => this.onInputChange(e.target.value)} />
+            </div>
         )
     }
 }
 
 export default SearchBar;
+
